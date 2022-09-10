cb61adf427ad35cfa490fbfa778dec3d952197a3
JNDJ0 <joaolucassmoreira0@gmail.com>
2022-09-10 16:44:33 -0500

    Quarto commit: modificacoes no .../controllers/index.js

--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -1,6 +1,7 @@
 const {  application  } = require('express');
 const express = require('express');
 const app = require('../../../../config/expressConfig');
+const listaDeMusicas = require('../models/musics');
 const router = express.Router();
 
 const music = require('../models/musics');
@@ -9,7 +10,7 @@ const music = require('../models/musics');
 //     res.status(200).send(music);
 // });
 
-router.get('/songs/', function(req,res){
+router.get('/songs', function(req,res){
     res.json(music);
 });
 
@@ -23,9 +24,36 @@ router.get('/songs/:name', function(req,res){
 });
 
 router.post('/',function(req,res){
-    const {nome, artista, genero, quantidadeDownloads} = req.body;
-    
+    const {nome,artista,genero,quantidadeDownloads} = req.body;
+
+
     res.status(200).json({nome, artista, genero, quantidadeDownloads});
 });
+
+router.put('/songs/:name', function(req,res){
+    const {name} = req.params;
+    const song = music.find(song => song.nome == name);
+
+    if(!song) return res.status(204).json();
+
+    const {nome,artista,genero,quantidadeDownloads} = req.body;
+
+    song.nome = nome;
+    song.artista = artista;
+    song.genero = genero;
+    song.quantidadeDownloads = quantidadeDownloads;
+
+    music.push(song);
+    res.json(song);
+
+});
+
+router.delete('/songs/:name', function(req,res){
+    const {name} = req.params
+    const songsFiltred = music.filter(song => song.nome != name);
+
+    res.json(songsFiltred);
+})
+
 
 module.exports = router;
