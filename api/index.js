c766709668ccca949cf08fb6f690185d3e222302
Gustavo Lima Mota <61586814+gstavomota@users.noreply.github.com>
2022-09-28 13:17:45 -0300

    teste de git

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./config/expressConfig');
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, console.log(`Server is running on ${port}`));
